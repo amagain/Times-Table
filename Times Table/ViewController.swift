bd700729741d8e7c0375f6e94c6891dc5cb93bd5
Kapil Amagain <kapil.amagain@gmail.com>
2015-02-18 21:15:07 +0545

    Initial Commit

---
+++ Times Table/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Times Table
+//
+//  Created by Kapil Amagain on 2/18/15.
+//  Copyright (c) 2015 Kapil Amagain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
